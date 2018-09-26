968ce646d659bfe78dfd9de932a17ef131c93406
Jose Pereira <josep@dei.uc.pt>
2018-09-26 17:21:57 +0100

    Changing to the consumer consumes the items

--- src/main/java/eubr/atmosphere/tma/planning/Main.java
+++ src/main/java/eubr/atmosphere/tma/planning/Main.java
@@ -20,6 +20,7 @@ public class Main
     public static void main( String[] args )
     {
         LOGGER.info("Hello World!");
+        runConsumer();
     }
 
     private static void runConsumer() {
