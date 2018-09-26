c00f84ea11a2f6bc59c24f88c0320b4a076775a0
Jose Pereira <josep@dei.uc.pt>
2018-09-26 17:13:32 +0100

    Adding the method to consume items from the topic

--- src/main/java/eubr/atmosphere/tma/planning/Main.java
+++ src/main/java/eubr/atmosphere/tma/planning/Main.java
@@ -1,7 +1,13 @@
 package eubr.atmosphere.tma.planning;
 
+import java.util.Iterator;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import eubr.atmosphere.tma.planning.utils.PropertiesManager;
 
 /**
  * Hello world!
@@ -9,10 +15,46 @@ import org.slf4j.LoggerFactory;
  */
 public class Main 
 {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main( String[] args )
     {
         LOGGER.info("Hello World!");
+    }
+
+    private static void runConsumer() {
+
+        Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
+        int noMessageFound = 0;
+
+        while (true) {
+
+          ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+          // 1000 is the time in milliseconds consumer will wait if no record is found at broker.
+          if (consumerRecords.count() == 0) {
+              noMessageFound++;
+
+              int maxNoMessageFoundCount =
+                      Integer.parseInt(PropertiesManager.getInstance().getProperty("maxNoMessageFoundCount"));
+              if (noMessageFound > maxNoMessageFoundCount)
+                // If no message found count is reached to threshold exit loop.
+                break;
+              else
+                  continue;
+          }
+
+          //print each record.
+          consumerRecords.forEach(record -> {
+              System.out.println("Record Key " + record.key());
+              System.out.println("Record value " + record.value());
+              System.out.println("Record partition " + record.partition());
+              System.out.println("Record offset " + record.offset());
+           });
+
+          // commits the offset of record to broker.
+          consumer.commitAsync();
+        }
+        consumer.close();
     }
 }
