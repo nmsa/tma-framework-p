27a9b4ea43fbe7ef69b255718dbadfeb6d013562
Jose Pereira <josep@dei.uc.pt>
2018-09-26 16:32:55 +0100

    Initial commit

---
+++ src/main/java/eu/atmosphere/tma/planning/Main.java
@@ -0,0 +1,18 @@
+package eu.atmosphere.tma.planning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	
+    public static void main( String[] args )
+    {
+        LOGGER.info("Hello World!");
+    }
+}
