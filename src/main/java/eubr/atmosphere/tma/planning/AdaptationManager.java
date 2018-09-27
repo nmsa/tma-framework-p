c9ed2c4da8876cafc7ddc2f03fe45654a0760f81
Jose Pereira <josep@dei.uc.pt>
2018-09-27 16:26:08 +0100

    Adding the AdaptationManager class

---
+++ src/main/java/eubr/atmosphere/tma/planning/AdaptationManager.java
@@ -0,0 +1,8 @@
+package eubr.atmosphere.tma.planning;
+
+public class AdaptationManager {
+
+    public static void performAdaptation() { 
+        System.out.println("Adaptation will be performed!");
+    }
+}
