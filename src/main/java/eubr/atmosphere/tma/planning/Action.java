e8cc2fc4f769d8f3620011ac6ec1196f42f1d2fb
Jose Pereira <josep@dei.uc.pt>
2018-09-28 10:31:17 +0100

    Adding the adapation to the topic-execute

---
+++ src/main/java/eubr/atmosphere/tma/planning/Action.java
@@ -0,0 +1,23 @@
+package eubr.atmosphere.tma.planning;
+
+public class Action {
+
+    private String action;
+    private int resourceId;
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+}
--- src/main/java/eubr/atmosphere/tma/planning/AdaptationManager.java
+++ src/main/java/eubr/atmosphere/tma/planning/AdaptationManager.java
@@ -1,8 +1,31 @@
 package eubr.atmosphere.tma.planning;
 
+import java.util.concurrent.ExecutionException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 public class AdaptationManager {
 
     public static void performAdaptation() { 
         System.out.println("Adaptation will be performed!");
+
+        Action action = new Action();
+        action.setAction("Test Action");
+        action.setResourceId(33);
+
+        JsonObject jsObj =  (JsonObject) new Gson().toJsonTree(action);
+        System.out.println(jsObj);
+
+        KafkaManager kafkaManager = new KafkaManager();
+        try {
+            kafkaManager.addItemKafka(jsObj.getAsString());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
