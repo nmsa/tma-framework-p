1f9bc79904e22644c0810dcafdd4f103bd38b101
Jose Pereira <josep@dei.uc.pt>
2019-03-05 11:57:14 +0000

    Implementation of the method to add the plan in the database

--- development/src/main/java/eubr/atmosphere/tma/planning/database/PlanManager.java
+++ development/src/main/java/eubr/atmosphere/tma/planning/database/PlanManager.java
@@ -1,14 +1,37 @@
 package eubr.atmosphere.tma.planning.database;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.mysql.jdbc.Statement;
+
+import eubr.atmosphere.tma.utils.DatabaseManager;
 
 public class PlanManager {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(PlanManager.class);
 
     public int saveNewPlan(Plan plan) {
-        LOGGER.info("saveNewPlam: to be implemented!");
+        String sql =
+                "INSERT INTO Plan(metricId, valueTime, qualityModelId, status) VALUES (?, ?, ?, ?)";
+        PreparedStatement ps;
+
+        try {
+            ps = DatabaseManager.getConnectionInstance().prepareStatement(
+                    sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, plan.getMetricId());
+            ps.setLong(2, plan.getValueTime());
+            ps.setInt(3, plan.getQualityModelId());
+            ps.setInt(4, plan.getStatus().ordinal());
+
+            DatabaseManager databaseManager = new DatabaseManager();
+            return databaseManager.execute(ps);
+        } catch (SQLException e) {
+            LOGGER.error("[ATMOSPHERE] Error when inserting an actuator in the database.", e);
+        }
         return -1;
     }
 }
