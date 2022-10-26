03dfdec3d2769237207447f52ed3fbf3138d2fab
João Ribeiro <49023295+Jodao@users.noreply.github.com>
2022-10-26 12:07:15 +0100

    Automation of Planning (#17)
    - Added an API for processing requests (create, read and delete) concerning adaptation rules
    - Automated the process of reading and changing the set of rules to apply. Now they are saved and read from the database and, if there are no rules at the database, a demo file will be used to create the set of rules

---
+++ development/src/main/java/eubr/atmosphere/tma/planning/api/util/Constants.java
@@ -0,0 +1,42 @@
+/**
+ * <b>ATMOSPHERE</b> - http://www.atmosphere-eubrazil.eu/
+ ***
+ * <p>
+ * <b>Trustworthiness Monitoring & Assessment Framework</b>
+ * Component: Admin API
+ * <p>
+ * Repository: https://github.com/eubr-atmosphere/tma-framework License:
+ * https://github.com/eubr-atmosphere/tma-framework/blob/master/LICENSE
+ * <p>
+ * <p>
+ */
+package eubr.atmosphere.tma.planning.api.util;
+
+/**
+ * This class holds the information of all the constants.
+ * <p>
+ * <p>
+ *
+ * @author Paulo Goncalves  <pgoncalves@student.dei.uc.pt>
+ * @author Jose A. D. Pereira  <josep@dei.uc.pt>
+ * @author Rui Silva <rfsilva@student.dei.uc.pt>
+ * @author Nuno Antunes     <nmsa@dei.uc.pt>
+ *
+ */
+public class Constants {
+
+    public static final int HTTPSUCESSCREATED = 201;
+    public static final int HTTPSUCESS = 200;
+    public static final int HTTPBADREQUEST = 400;
+    public static final int HTTPMETHODNOTALLOWED = 405;
+    public static final int HTTPURLNOTFOUND = 404;
+    public static final int HTTPSERVERERROR = 500;
+    public static final int HTTPUNSUPPORTEDMEDIATYPE = 415;
+
+    public static final int MAXKEYSYZE = 600;
+    public static final int IDSPERPARTNER = 1000;
+    public static final int ERROR = -1;
+    public static final int WARNING = 0;
+    public static final int SUCCESS = 1;
+
+}
